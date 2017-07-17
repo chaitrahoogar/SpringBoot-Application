e9c5e16ab81f8164d17e750cee4cbbcf9a127507
Chaitra <chaitra.hoogar@hashworks.co>
2017-07-17 13:08:59 +0530

    first commit

---
+++ src/main/java/com/hashworks/spring1/HomeController.java
@@ -0,0 +1,12 @@
+package com.hashworks.spring1;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController 
+public class HomeController {
+	@RequestMapping("/hello")  
+    public String hello(){  
+        return"Hello!";  
+    }  
+}
